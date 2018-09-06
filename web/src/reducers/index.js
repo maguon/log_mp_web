f49a96586e7a689f4201b5f4114c47c236ea643b
yjyvsdream <jingyuan.yu@myxxjs.com>
2018-09-06 11:03:42 +0800

    新建：追加 询价相关机能用：EnquiryReducer

--- web/src/reducers/index.js
+++ web/src/reducers/index.js
@@ -1,11 +1,13 @@
 import { combineReducers } from 'redux'
 import { reducer as reduxFormReducer } from 'redux-form';
 
-import UserReducer from './main/UserReducer'
-import LoginReducer from './layout/LoginReducer'
 import HeaderReducer from './layout/HeaderReducer'
+import LoginReducer from './layout/LoginReducer'
+
+import EnquiryReducer from './main/EnquiryReducer'
+import UserReducer from './main/UserReducer'
 
 export default combineReducers({
     form: reduxFormReducer,
-    UserReducer,LoginReducer,HeaderReducer
+    UserReducer,LoginReducer,HeaderReducer,EnquiryReducer
 })

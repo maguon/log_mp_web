c1880f8e8d972b8103054028929c25c7175c6b41
yjyvsdream <jingyuan.yu@myxxjs.com>
2018-09-06 10:52:43 +0800

    新建：追加HeaderActionType

--- web/src/actionTypes/index.js
+++ web/src/actionTypes/index.js
@@ -1,7 +1,10 @@
-import * as LoginActionType  from './layout/LoginActionType'
 import * as HeaderActionType  from './layout/HeaderActionType'
+import * as LoginActionType  from './layout/LoginActionType'
+
+import * as EnquiryActionType  from './main/EnquiryActionType'
 
 export  {
     LoginActionType,
-    HeaderActionType
+    HeaderActionType,
+    EnquiryActionType
 }

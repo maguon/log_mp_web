352ff6f59e5e87e388fb81e7ef7aff77f711e231
yjyvsdream <jingyuan.yu@myxxjs.com>
2018-09-06 11:18:22 +0800

    新建：格式化代码并 追加 部分注释

--- web/src/components/login.jsx
+++ web/src/components/login.jsx
@@ -1,36 +1,49 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-import {Login,Register,ResetPassword} from './layout/index';
+import {Login, Register, ResetPassword} from './layout/index';
 
-import { HashRouter as Router, Route, Link } from "react-router-dom";
-import { Provider } from 'react-redux';
+import {HashRouter as Router, Route} from "react-router-dom";
+import {Provider} from 'react-redux';
 import ReduxThunk from 'redux-thunk'
 
 import reducers from '../reducers'
-import {applyMiddleware,createStore,compose} from 'redux';
+import {applyMiddleware, createStore, compose} from 'redux';
 
+/**
+ * 数据仓库(存放全局变量, 将reducers注入store)
+ */
 const store = compose(
     applyMiddleware(ReduxThunk)
-)(createStore)(reducers,window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__())
-const routes = [
-    {
+)(createStore)(reducers, window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__());
 
-        path:"/",
+/**
+ * 登录画面路由配置。
+ */
+const routes = [
+    // 登录画面
+    {
+        path: "/",
         exact: true,
-        component : Login
+        component: Login
     },
+    // 注册用户
     {
         path: "/register",
         exact: true,
-        component : Register
+        component: Register
     },
+    // 重置密码
     {
         path: "/reset",
         exact: true,
-        component : ResetPassword
+        component: ResetPassword
     }
 ];
+
+/**
+ * 主画面。(store 用来存放全局变量)
+ */
 const body = (
     <Provider store={store}>
         <Router hashType={"hashbang"}>
@@ -46,9 +59,11 @@ const body = (
             </div>
         </Router>
     </Provider>
+);
 
-)
-
+/**
+ * 主渲染。(根节点 渲染 组件)
+ */
 ReactDOM.render(
     body,
     document.getElementById('root')

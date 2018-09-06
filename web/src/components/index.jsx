fd2dd0caef7356161c07bf102db180944b5a6269
yjyvsdream <jingyuan.yu@myxxjs.com>
2018-09-06 11:23:50 +0800

    新建：格式化代码并 追加 部分注释以及样式

--- web/src/components/index.jsx
+++ web/src/components/index.jsx
@@ -1,27 +1,36 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-import {Footer,Header,Container,Nav} from './layout/index';
+import {Footer, Header, Container} from './layout/index';
 
-import { Provider } from 'react-redux';
+import {Provider} from 'react-redux';
 import ReduxThunk from 'redux-thunk'
 import reducers from '../reducers'
-import {applyMiddleware,createStore,compose} from 'redux';
+import {applyMiddleware, createStore, compose} from 'redux';
 
+/**
+ * 数据仓库(存放全局变量, 将reducers注入store)
+ */
 const store = compose(
     applyMiddleware(ReduxThunk)
-)(createStore)(reducers,window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__())
+)(createStore)(reducers, window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__());
+
+/**
+ * 主画面。(store 用来存放全局变量)
+ */
 const body = (
     <Provider store={store}>
-        <div>
+        <div style={{height: '100%'}}>
             <Header/>
             <Container/>
             <Footer/>
         </div>
     </Provider>
+);
 
-)
-
+/**
+ * 主渲染。(根节点 渲染 组件)
+ */
 ReactDOM.render(
     body,
     document.getElementById('root')

e966b88f9ee2ed0bb5357f13b083ac14ff249820
yjyvsdream <jingyuan.yu@myxxjs.com>
2018-09-06 11:01:38 +0800

    新建：追加 询价相关机能用：EnquiryAction

--- web/src/actions/index.js
+++ web/src/actions/index.js
@@ -1,3 +1,4 @@
-export UserAction  from './main/UserAction'
 export HeaderAction  from './layout/HeaderAction'
 export LoginAction  from './layout/LoginAction'
+export EnquiryAction  from './main/EnquiryAction'
+export UserAction  from './main/UserAction'

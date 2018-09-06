aedee3a16b2825b04e19bbf248d3a27bc6be4035
yjyvsdream <jingyuan.yu@myxxjs.com>
2018-09-06 11:30:15 +0800

    新建：追加 样式

--- web/src/components/layout/Container.jsx
+++ web/src/components/layout/Container.jsx
@@ -1,32 +1,32 @@
 import React from 'react';
 
-import {Panel,Setting,User,UserDetail} from '../main/index';
-import { HashRouter as Router, Route, Link } from "react-router-dom";
+import {Panel, Setting, User, UserDetail} from '../main/index';
+import {HashRouter as Router, Route, Link} from "react-router-dom";
 
 import {fileHost} from '../../config/HostConfig';
 
-import { connect } from 'react-redux';
+import {connect} from 'react-redux';
+
 const routes = [
     {
-
-        path:"/user",
+        path: "/user",
         exact: true,
-        component : User
-    },
-    {
-        path: "/setting",
-        exact: true,
-        component : Setting
-    },
-    {
-        path: "/panel",
-        exact: true,
-        component : Panel
+        component: User
     },
     {
         path: '/user/:id',
         exact: true,
-        component : UserDetail
+        component: UserDetail
+    },
+    {
+        path: "/setting",
+        exact: true,
+        component: Setting
+    },
+    {
+        path: "/panel",
+        exact: true,
+        component: Panel
     }
 ];
 
@@ -35,17 +35,17 @@ class Container extends React.Component {
     constructor(props) {
         super(props);
     }
-    componentDidMount(){
 
-
+    componentDidMount() {
     }
+
     render() {
         const {headerReducer} = this.props;
         console.log(headerReducer.userInfo);
-        const avatarUrl = "http://"+fileHost+"/api/image/"+headerReducer.userInfo.avatar_image ;
+        const avatarUrl = "http://" + fileHost + "/api/image/" + headerReducer.userInfo.avatar_image;
         return (
             <Router hashType={"hashbang"}>
-                <div>
+                <div className="context-height">
                     <ul id="slide-out" className="sidenav">
                         <li>
                             <div className="user-view blue-grey">
@@ -61,12 +61,18 @@ class Container extends React.Component {
                                 </a>
                             </div>
                         </li>
-                        <li className="no-padding"> <Link to="/panel"  className="collapsible-header"><i className="mdi mdi-cards-variant"></i>面板</Link></li>
-                        <li><div class="divider"></div></li>
+                        <li className="no-padding"><Link to="/panel" className="collapsible-header"><i
+                            className="mdi mdi-cards-variant"></i>面板</Link></li>
+                        <li>
+                            <div class="divider"></div>
+                        </li>
 
-                        <li className="no-padding"> <Link to="/user" className="collapsible-header"><i className="mdi mdi-account-group"></i>用户</Link></li>
+                        <li className="no-padding"><Link to="/user" className="collapsible-header"><i
+                            className="mdi mdi-account-group"></i>用户</Link></li>
 
-                        <li><div class="divider"></div></li>
+                        <li>
+                            <div class="divider"></div>
+                        </li>
                         <li className="no-padding">
                             <ul className="collapsible collapsible-accordion">
                                 <li>
@@ -92,29 +98,25 @@ class Container extends React.Component {
                     ))}
                 </div>
             </Router>
-
-
-
         )
-
-
     }
 }
+
 const mapStateToProps = (state) => {
     return {
         headerReducer: state.HeaderReducer
     }
-}
+};
 
 const mapDispatchToProps = (dispatch) => ({
 
     getUserDetail: (userId) => {
-        dispatch(headerAction.getUserDetail({userId:userId}))
+        dispatch(headerAction.getUserDetail({userId: userId}))
     },
-    logout : ()=>{
+    logout: () => {
         dispatch(headerAction.logout())
     }
 
-})
+});
 
 export default connect(mapStateToProps, mapDispatchToProps)(Container)

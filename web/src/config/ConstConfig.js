550a94a562372502ca78b75b4a138a5410c192ea
yjyvsdream <jingyuan.yu@myxxjs.com>
2018-09-18 16:42:51 +0800

    新建：追加 日期组件 汉化用基本设定

--- web/src/config/ConstConfig.js
+++ web/src/config/ConstConfig.js
@@ -48,3 +48,18 @@ export const YES_NO = [
         label: "是"
     }
 ];
+
+export const DATE_PICKER_OPTION = {
+    // selectMonths: true,
+    // selectYears: 15,
+    format: 'yyyy-mm-dd',
+    weekdaysLetter: ['日', '一', '二', '三', '四', '五', '六'],
+    today: '今天',
+    clear: '清除',
+    close: '关闭',
+    monthsFull: ['一月', '二月', '三月', '四月', '五月', '六月', '七月', '八月', '九月', '十月', '十一月', '十二月'],
+    monthsShort: ['1月', '2月', '3月', '4月', '5月', '6月', '7月', '8月', '9月', '10月', '11月', '12月'],
+    weekdaysFull: ['周日', '周一', '周二', '周三', '周四', '周五', '周六'],
+    closeOnSelect: true, // Close upon selecting a date,
+    // container: undefined, // ex. 'body' will append picker to body
+};

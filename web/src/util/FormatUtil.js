3cc6d7e8c86a45286d3d3fe75b8f657dbf694bf3
yjyvsdream <jingyuan.yu@myxxjs.com>
2018-09-19 14:47:59 +0800

    新建：更新注释

--- web/src/util/FormatUtil.js
+++ web/src/util/FormatUtil.js
@@ -1,17 +1,11 @@
 /**
  * 对Date的扩展，将 Date 转化为指定格式的String
  * @param date 要格式化的日期
- * @param format 指定格式
+ * @param format 指定格式 例：FormatUtil.DateFormat(new Date(),'yyyy-MM-dd hh:mm:ss.S q') ==> 2018-09-19 13:33:17.148 3
  */
 export const DateFormat = (date, format) => {
     // 月(M)、日(d)、小时(h)、分(m)、秒(s)、季度(q) 可以用 1-2 个占位符，
     // 年(y)可以用 1-4 个占位符，毫秒(S)只能用 1 个占位符(是 1-3 位的数字)
-
-    // 例子：
-    // FormatUtil.DateFormat(date,'yyyy-MM-dd hh:mm:ss.S q') ==> 2018-09-19 13:33:17.148 3
-    // (new Date()).Format("yyyy-MM-dd hh:mm:ss.S") ==> 2006-07-02 08:09:04.423
-    // (new Date()).Format("yyyy-M-d h:m:s.S")      ==> 2006-7-2 8:9:4.18
-
     Date.prototype.Format = function (fmt) { //author: meizz
         let o = {
             "M+": this.getMonth() + 1,                     //月份
@@ -35,12 +29,9 @@ export const DateFormat = (date, format) => {
 // /**
 //  * 数字格式化。
 //  * @param number 数字
-//  * @param format
 //  * @returns {string} 本地标准格式
 //  */
-// export const NumberFormat = (number, format) => {
-//     // 保留小数点后2位
-//     // number.toFixed(2)
+// export const NumberFormat = (number) => {
 //     return new Intl.NumberFormat().format(number);
 // };
 
@@ -48,10 +39,11 @@ export const DateFormat = (date, format) => {
  * 数字格式化。
  * @param number 数字
  * @param decimals 保留小数位数
- * @returns {string} 标准格式
+ * @returns {string} 标准格式 例：FormatUtil.NumberFormat(123456.789, 2) ==> 123,456.79
  */
 export const NumberFormat = (number, decimals) => {
     decimals = typeof undefined === 'undefined'? 0 : decimals;
+    // 保留指定小数点后位数，并分割数组
     let x = number.toFixed(decimals).split('.');
     let x1 = x[0];
     let x2 = x.length > 1 ? '.' + x[1] : '';
@@ -64,7 +56,7 @@ export const NumberFormat = (number, decimals) => {
 /**
  * 货币格式化。
  * @param amount 货币
- * @returns {string} 美元货币格式
+ * @returns {string} 美元货币格式  例：FormatUtil.CurrencyFormat(123456.789) ==> $123,456.79
  */
 export const CurrencyFormat = (amount) => {
     return new Intl.NumberFormat('en-US', { style: 'currency', currency: 'USD' }).format(amount);

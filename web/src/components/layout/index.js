5e7899776ff8b17dd89ca8d9e8201f8fbab7693a
yjyvsdream <jingyuan.yu@myxxjs.com>
2018-09-28 10:27:45 +0800

    新建：去 无用代码

--- web/src/components/layout/index.js
+++ web/src/components/layout/index.js
@@ -2,6 +2,5 @@ export Container from "./Container";
 export Footer from "./Footer";
 export Header from "./Header";
 export Login from "./Login";
-export Nav from "./Nav";
 export Register from "./Register";
 export ResetPassword from "./ResetPassword";

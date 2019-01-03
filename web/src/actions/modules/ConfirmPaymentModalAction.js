918ed443d09715fcaba48dfa01da7f336bab4aff
yjyvsdream <jingyuan.yu@myxxjs.com>
2019-01-03 14:24:46 +0800

    修改：对应接口变更

--- web/src/actions/modules/ConfirmPaymentModalAction.js
+++ web/src/actions/modules/ConfirmPaymentModalAction.js
@@ -26,21 +26,31 @@ export const confirmPayment = () => async (dispatch, getState) => {
         // 金额
         const paymentMoney = getState().ConfirmPaymentModalReducer.paymentMoney;
 
-        // 基本url
-        let url = apiHost + '/api/admin/' + localUtil.getSessionItem(sysConst.USER_ID)
-            + '/payment/' + paymentId + '//bankTransfer/amount/' + paymentMoney;
-        let res = await httpUtil.httpPut(url, {});
-        if (res.success === true) {
-            $('#confirmPaymentModal').modal('close');
-            swal("支付审核成功", "", "success");
-            // 保存成功后，重新检索画面数据
-            if (prePage === 'payment') {
-                dispatch(paymentManagerAction.getPaymentList());
-            } else {
-                dispatch(paymentManagerDetailAction.getPaymentInfo(paymentId));
+        if (paymentMoney === '') {
+            swal('保存失败', '请输入金额！', 'warning');
+        } else {
+            // 基本url
+            let url = apiHost + '/api/admin/' + localUtil.getSessionItem(sysConst.USER_ID)
+                + '/payment/' + paymentId + '/bankTransfer/paymentReview';
+
+            const params = {
+                totalFee: paymentMoney,
+                status: sysConst.PAYMENT_STATUS[1].value
+            };
+
+            let res = await httpUtil.httpPut(url, params);
+            if (res.success === true) {
+                $('#confirmPaymentModal').modal('close');
+                swal("支付审核成功", "", "success");
+                // 保存成功后，重新检索画面数据
+                if (prePage === 'payment') {
+                    dispatch(paymentManagerAction.getPaymentList());
+                } else {
+                    dispatch(paymentManagerDetailAction.getPaymentInfo(paymentId));
+                }
+            } else if (res.success === false) {
+                swal('支付审核失败', res.msg, 'warning');
             }
-        } else if (res.success === false) {
-            swal('支付审核失败', res.msg, 'warning');
         }
     } catch (err) {
         swal('操作失败', err.message, 'error');
